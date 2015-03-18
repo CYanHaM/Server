ac690e635f36afe9ba245ed7659c93bdd496067e
turtlology <504350677@qq.com>
2015-03-18 18:47:11 +0800

    Enum

--- src/TypeEnum/PlayerTechEnum.java
+++ src/TypeEnum/PlayerTechEnum.java
@@ -1,5 +1,34 @@
 package TypeEnum;
 
 public enum PlayerTechEnum {
-
+	name,               //姓名
+	season,             //赛季
+	team,               //所属球队
+	gameNum,            //参赛场数
+	startingNum,        //首发场数
+	rebound,            //篮板数
+	secondaryAttack,    //助攻数
+	time,               //在场时间
+	shotInRate,         //投篮命中率
+	threeshotInRate,    //三分命中率
+	penaltyShotInRate,  //罚球命中率
+	offensiveNum,       //进攻数
+	defensiveNum,       //防守数
+	steal,              //抢断数
+	blockShot,          //盖帽数
+	fault,              //失误数
+	foul,               //犯规数
+	score,              //得分
+	effiency,           //效率
+	GmScEffiency,       //GmSc效率值
+	trueShotInRate,     //真实命中率
+	shootingEffiency,   //投篮效率
+	reboundRate,        //篮板率
+	offensiveReboundRate,//进攻篮板率
+	defensiveReboundRate,//防守篮板率
+	secondaryAttackRate,//助攻数
+	stealRate,          //抢断数
+	blockShotRate,      //盖帽数
+	faultRate,          //失误数	
+	usageRate,          //使用率
 }
--- src/TypeEnum/TeamTechEnum.java
+++ src/TypeEnum/TeamTechEnum.java
@@ -1,5 +1,32 @@
 package TypeEnum;
 
 public enum TeamTechEnum {
-
+	name,                            //球队名称
+	season,                          //赛季
+	gameNum,                         //比赛场数
+	shotInNum,                       //投篮命中数
+	shotNum,                         //投篮出手数
+	threeShotInNum,                  //三分命中数
+	threeShotNum,                    //三分出手数
+	penaltyShotInNum,                //罚球命中数
+	penaltyShotNum,                  //发球出手数
+	offensiveRebound,                //前场篮板数
+	defensiveRebound,                //后场篮板数
+	rebound,                         //总篮板数
+	secondaryAttack,                 //助攻数
+	steal,                           //抢断数
+	blockShot,                       //盖帽数
+	fault,                           //失误数
+	foul,                            //犯规数
+	score,                           //比赛得分
+	shotInRate,                      //投篮命中率
+	threeShotInRate,                 //三分命中率
+	penaltyShotInRate,               //罚球命中率
+	winningRate,                     //胜率
+	offensiveRound,                  //进攻回合
+	offensiveEffiency,               //进攻效率
+	defensiveEffiency,               //防守效率
+	reboundEffiency,                 //篮板效率
+	stealEffiency,                   //抢断效率
+	secondaryAttackEffiency;         //助攻效率
 }
