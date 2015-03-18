68ed002b5b7bbfcd96f098f350f6b7cb6af34401
Mao-xy <15850563830@139.com>
2015-03-18 20:10:48 +0800

    根据姓名查找player

---
+++ src/data/TeamData.java
@@ -0,0 +1,15 @@
+package data;
+
+import PO.TeamPO;
+import dataservice.TeamDataService;
+
+public class TeamData implements TeamDataService{
+
+	@Override
+	public TeamPO find(TeamPO tpo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
---
+++ src/data/TeamTechData.java
@@ -0,0 +1,23 @@
+package data;
+
+import java.util.ArrayList;
+
+import PO.TeamTechPO;
+import TypeEnum.TeamTechEnum;
+import dataservice.TeamTechDataService;
+
+public class TeamTechData implements TeamTechDataService{
+
+	@Override
+	public ArrayList<TeamTechPO> ascend(TeamTechEnum type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<TeamTechPO> descend(TeamTechEnum type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
