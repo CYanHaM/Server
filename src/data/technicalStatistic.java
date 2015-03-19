6ecf30d56fe613a1d99a9322e527ece44798cfeb
admin <admin@172.25.134.117>
2015-03-19 19:42:28 +0800

    123

--- src/data/technicalStatistic.java
+++ src/data/technicalStatistic.java
@@ -1,7 +1,17 @@
 package data;
 
+import java.util.ArrayList;
+
+import PO.TeamTechPO;
+
 public class technicalStatistic {
+	public ArrayList<TeamTechPO> teamTechGenerating(ArrayList<TeamTechPO> ttplist){
+		
+		
+		return ttplist;
 	
+		
+	}
 	
 
 }
