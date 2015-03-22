8e22b683d850b355880a0d9330987ff10b58b173
turtlology <504350677@qq.com>
2015-03-22 15:29:53 +0800

    RMI finished

--- src/dataservice/PlayerTechDataService.java
+++ src/dataservice/PlayerTechDataService.java
@@ -1,19 +1,21 @@
 package dataservice;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 import PO.PlayerPO;
 import PO.PlayerTechPO;
 import TypeEnum.PlayerTechEnum;
 
-public interface PlayerTechDataService {
-	public ArrayList<PlayerTechPO> ascend(PlayerTechEnum type);
+public interface PlayerTechDataService extends Remote{
+	public ArrayList<PlayerTechPO> ascend(PlayerTechEnum type)throws RemoteException;
 	//按照type所示条件升序排序
 	
-	public ArrayList<PlayerTechPO> descend(PlayerTechEnum type);
+	public ArrayList<PlayerTechPO> descend(PlayerTechEnum type)throws RemoteException;
 	//按照type所示条件降序排序
 	
-	public ArrayList<PlayerTechPO> sift(String position, String division, PlayerTechPO ptpo);
+	public ArrayList<PlayerTechPO> sift(String position, String division, PlayerTechPO ptpo)throws RemoteException;
 	/* 根据条件筛选并排序
 	 * 可用作筛选条件的有：球员位置，球员联盟
 	 * 可用作排序条件的有：球员名称，所属球队，参赛场数，先发场数，篮板数，助攻数，在场时间，投篮命中率，三分命中率，罚球命中率，进攻数，防守数，抢断数，盖帽数，失误数，犯规数，得分，效率，GmSc效率值，真实命中率，投篮效率，篮板率，进攻篮板率，防守篮板率，助攻率，抢断率，盖帽率，失误率，使用率
--- src/rmi/StartTeamTechService.java
+++ src/rmi/StartTeamTechService.java
@@ -1,5 +1,60 @@
 package rmi;
 
-public class StartTeamTechService {
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NoSuchObjectException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
 
+import data.TeamTechData;
+import dataservice.TeamTechDataService;
+
+public class StartTeamTechService {
+	/**
+	 * 注册远程接口PlayerDataService
+	 * @author Ni_Xiaofan
+	 * @date 2014年12月23日
+	 * @version 1
+	 */
+
+	//定义公开的IP地址和端口，方便多用户登录，并有set与get方法
+	String host;
+	String port;
+	Remote reg=null;
+	public StartTeamTechService() throws RemoteException, MalformedURLException{
+		ServerURL server = new ServerURL();
+		host = server.getHost();
+		System.out.println("host:"+host);
+		port = server.getTeamTechPort();
+		//把端口和远程对象UserData注册到RMI注册服务器上
+		reg=LocateRegistry.createRegistry(Integer.valueOf(port));
+		TeamTechDataService teamtechdata = new TeamTechData();
+		//绑定的URL标准格式为：rmi://host:port/name(协议名rmi可以省略)
+		Naming.rebind("//"+host+":"+port+"/TeamData", teamtechdata);
+		System.out.println(">>>>>INFO:远程TeamTechData对象绑定成功！");
+	}
+	
+	public void shutdown(){
+		try {
+			UnicastRemoteObject.unexportObject(reg, true);
+		} catch (NoSuchObjectException e) {
+			e.printStackTrace();
+		}
+		System.out.println("port shutdown successfully!");
+	}
+	
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public String getPort() {
+		return port;
+	}
+	public void setPort(String port) {
+		this.port = port;
+	}
 }
