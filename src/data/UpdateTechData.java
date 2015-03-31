cc275f1cc22bb23eb56ebf221cd3e4991a4242ee
Mao-xy <15850563830@139.com>
2015-03-31 20:35:32 +0800

    增加更新数据库的接口

---
+++ src/data/UpdateTechData.java
@@ -0,0 +1,18 @@
+package data;
+
+import java.util.ArrayList;
+
+import PO.PlayerTechPO;
+import PO.TeamTechPO;
+
+public class UpdateTechData implements UpdateService {
+	
+	public void updatePlayerTech(ArrayList<PlayerTechPO> list){
+		
+	}
+	
+	public void updateTeamTech(ArrayList<TeamTechPO> list){
+		
+	}
+
+}
