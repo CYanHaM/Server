99a9e6d058e3849ea10f63c3255bb464b7ab3021
turtlology <504350677@qq.com>
2015-04-01 21:55:15 +0800

    datservice string

--- src/dataservice/TeamTechDataService.java
+++ src/dataservice/TeamTechDataService.java
@@ -8,9 +8,9 @@ import PO.TeamTechPO;
 import TypeEnum.TeamTechEnum;
 
 public interface TeamTechDataService extends Remote{
-	public ArrayList<TeamTechPO> ascend(TeamTechEnum type)throws RemoteException;
+	public ArrayList<TeamTechPO> ascend(String type)throws RemoteException;
 	// 按照type 所示条件升序排序
 	
-	public ArrayList<TeamTechPO> descend(TeamTechEnum type)throws RemoteException;
+	public ArrayList<TeamTechPO> descend(String type)throws RemoteException;
 	//按照type所示条件降序排序
 }
